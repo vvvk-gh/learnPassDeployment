3b8cd737f8218ee0265d64b80abe83be9597c99e
vvvk-gh <vamsivutukuri1997@gmail.com>
2020-07-26 03:05:33 +0530

    div boxes and audios

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+
 const http = require('http')
+const socketio = require('socket.io')
+
+//creating an experss app into http app
+const server = http.createServer(app)
+//attaching socket to the sever
+const io = socketio(server)
+
+io.on('connection' , (socket) => {
+        console.log(`Connected to the socket id: ${socket.id}` )
+})
+
+
 //for heroku port
 const SERVER_PORT = process.env.PORT || 5656
 
